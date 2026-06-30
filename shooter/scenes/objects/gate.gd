d87296267288b787ff5a371c493c9fc2981568d3
ganeshkarthik09 <vislavathganeshkarthik@gmail.com>
2026-06-30 15:14:21 +0530

    refactor: consolidate game portfolio to include shooter and jumping-japang side-by-side

--- shooter/scenes/objects/gate.gd
+++ shooter/scenes/objects/gate.gd
@@ -3,16 +3,5 @@ extends StaticBody2D
 signal player_entered_gate(body)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	#print("Body has entered") # Replace with function body.
+func _on_area_2d_body_entered(body):
 	player_entered_gate.emit(body)
